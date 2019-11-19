f29e48efe3cf0d9a550f2bd0f89015724066559a
Tiffany <tiffanyandressa15@gmail.com>
2019-11-19 16:23:22 -0200

    cobrinha iniciada

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,13 @@
+const tamanho = 10;
+
 function setup() {
   createCanvas(400,400);
+  cobrinha = new cobra();
+  
 }
 
 function draw() {
-  background(51)
+  background(51);
+  cobrinha.show();
+  cobrinha.update();
 }
