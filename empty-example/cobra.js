48afb31bcbc5614d7dcfcec47894d737fad27b8a
Laboratório 02B Fatec <Lab02b@fatec.local>
2019-11-20 17:54:15 -0200

    frutinha

--- empty-example/cobra.js
+++ empty-example/cobra.js
@@ -3,13 +3,19 @@ function cobra(){
     this.y = 0;
     this.xvelocidade = 1;
     this.yvelocidade = 0;
-
+        
     this.update = function(){
-        this.x += this.xvelocidade*tamanho;
-        this.y +=this.yvelocidade*tamanho;
-    }
+    this.x += this.xvelocidade*tamanho;
+    this.y += this.yvelocidade*tamanho;
+        }
+    
     this.show = function(){
-        fill(255)
-        rect(this.x,this.y,tamanho,tamanho)
+        fill(255);
+        rect(this.x, this.y, tamanho, tamanho);
+        }
+    this.dir = function(x,y){
+    this.xvelocidade = x;
+    this.yvelocidade = y;
+        }
+        
     }
-}
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,14 +1,36 @@
 const tamanho = 10;
+let cobrinha;
 
 function setup() {
   createCanvas(400,400);
   cobrinha = new cobra();
-  frameRate(2);
-  
+  frameRate(15);
+  posicaoComida();
 }
-
 function draw() {
   background(51);
   cobrinha.show();
   cobrinha.update();
+  posicaoComida();
+  fill(255,0,100);
+  rect(comida.x,comida.y,tamanho,tamanho);
+}
+function posicaoComida(){
+  var coluna = floor(width/tamanho);
+  var fila = floor(height/tamanho);
+  comida = createVector(floor(random(coluna)),floor(random(fila)));
+  comida.mult(tamanho);
+}
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    cobrinha.dir(0,-1);
+  }else if(keyCode === DOWN_ARROW){
+    cobrinha.dir(0,1);
+  }else if(keyCode === RIGHT_ARROW){
+    cobrinha.dir(1,0);
+  }else if(keyCode === LEFT_ARROW){
+    cobrinha.dir(-1,0);
+  }
+  
+
 }
